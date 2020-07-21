54d43ecbc75e589b8f447b400bfc2d51bfbec06f
Mathan Ram Marikani <meetmemathanram@yahoo.com>
2020-07-22 00:52:58 +0530

    added recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes=[];
+  recipe: Recipe[] =[
+    new Recipe('Test recipe', 'This is simply a test desc', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
